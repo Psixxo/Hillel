bda4cecee9761e07e94a7d97685152d446ae8838
Psixxo <68906788+Psixxo@users.noreply.github.com>
2025-09-26 21:18:54 +0300

    Hillel

---
+++ src/main/java/homework2/Work2.java
@@ -0,0 +1,28 @@
+package main.java.homework2;
+
+public class Work2 {
+    public static void main(String[] args) {
+
+        int order1 = 1;
+        String client1 = "Alice";
+        String product1 = "Smartphone";
+        double price1 = 305.99;
+        String address1 = "Moon Street, 10";
+
+        System.out.println("Order No " + order1 + " Client: " + client1);
+        System.out.println("Product: " + product1);
+        System.out.println("Price EUR " + price1);
+        System.out.println("Address: " + address1);
+
+        int order2 = 2;
+        String client2 = "Tom";
+        String product2 = "Laptop";
+        double price2 = 570.95;
+        String address2 = "Terra Street, 17";
+
+        System.out.println("Order No " + order2 + " Client: " + client2);
+        System.out.println("Product: " + product2);
+        System.out.println("Price EUR " + price2);
+        System.out.println("Address: " + address2);
+    }
+}
---
+++ src/main/java/lesson2/App.java
@@ -0,0 +1,20 @@
+package main.java.lesson2;
+
+public class App {
+    public static void main(String[] args) {
+
+        // byte someByte = 127;
+        // byte someByte = 0b1101;
+        byte someByte = 0x1f;
+        System.out.println(someByte);
+
+        short someShort = 0x1f; //4441
+        int someInt = 0x1f; //123123
+        long someLong = 12312312312L;
+
+        float someFloat = 1.11231f;
+        double someDouble = 1.1234234d;
+        char someChar = 'A';
+        System.out.println(someChar);
+    }
+}
